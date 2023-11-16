67f5a41c90eb2d9e871bee9fd4bf8b1a25346060
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-11-16 14:11:12 -0300

    feat: added update profile function and logic

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,18 +1,11 @@
 import { apiSlice } from "./apiSlice";
 const USERS_URL = "/api/users";
 
-export const usersApiSlice = apiSlice.injectEndpoints({
+export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}/auth`,
-        method: "POST",
-        body: data,
-      }),
-    }),
-    register: builder.mutation({
-      query: (data) => ({
-        url: `${USERS_URL}`,
         method: "POST",
         body: data,
       }),
@@ -23,8 +16,26 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         method: "POST",
       }),
     }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation } =
-  usersApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useUpdateUserMutation,
+} = userApiSlice;
