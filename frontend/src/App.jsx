0b5ecfeeb3d7de677c986e34951eddf3504fc1a7
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-11-16 11:56:11 -0300

    feat: added basic forms, components and component level state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
-
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 const App = () => {
   return (
     <>
       <Header />
-      <h1>Mern auth</h1>
+      <Container className="my-2">
+        {" "}
+        <Outlet />
+      </Container>
     </>
   );
 };
