b6160e67fb676087bdf2c812cb63a8150323d545
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-11-23 10:55:24 -0300

    chore: testing base url configs

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({
+  baseUrl:
+    process.env.NODE_ENV === "production"
+      ? "https://mern-auth-production-cae6.up.railway.app/"
+      : "",
+});
 
 export const apiSlice = createApi({
   baseQuery,
