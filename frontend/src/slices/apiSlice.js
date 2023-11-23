b875b6aa349719e5332251e06451157e2af9fa10
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-11-23 12:04:20 -0300

    chore: revert environment validation changes

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,11 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({
-  baseUrl:
-    process.env.NODE_ENV === "production"
-      ? "https://mern-auth-production-cae6.up.railway.app/"
-      : "",
-});
+const baseQuery = fetchBaseQuery({ baseUrl: "" });
 
 export const apiSlice = createApi({
   baseQuery,
