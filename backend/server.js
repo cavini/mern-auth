4cd89270f0dad7b2665c1a820eedc2d8e9afd9a5
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-11-23 12:50:12 -0300

    chore: added cors solution, whitelisted my domain on server

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ import connectDB from "./config/db.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 connectDB();
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +23,13 @@ app.use("/api/users", userRoutes);
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "frontend/dist")));
+  const corsOptions = {
+    origin: "https://precious-salamander-61cc62.netlify.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  };
+
+  app.use(cors(corsOptions));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
